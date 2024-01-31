15fb33194d72ff5bea57f0d5380917f35f57bd2f
Etienne Vautherin <etienne@vautherin.com>
2024-01-31 17:19:33 +0100

    CLMonitor

--- ProdigesMap/ProdigesModel.swift
+++ ProdigesMap/ProdigesModel.swift
@@ -8,13 +8,43 @@
 import Foundation
 import CoreLocation
 
-class ProdigesModel {
+class ProdigesModel : NSObject {
     static let shared = ProdigesModel()
     let center = CLLocationCoordinate2D(latitude: 48.9355351, longitude: 2.3030026)
     private let manager = CLLocationManager()
+//    var monitor: CLMonitor!
     
-    init() {
+    override init() {
+        super.init()
+        
+        manager.delegate = self
         manager.requestWhenInUseAuthorization()
+    }
+    
+}
+
+
+extension ProdigesModel: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print("\(manager.authorizationStatus)")
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            Task {
+                let monitor = await CLMonitor("monitorName")
+                let condition = CLMonitor.CircularGeographicCondition(
+                    center: center,
+                    radius: 50
+                )
+                await monitor.add(condition, identifier: "Condition")
+                
+                for identifier in await monitor.identifiers {
+                    print("Condition: \(identifier)")
+                }
+                
+                for try await event in await monitor.events {
+                    print("state:\(event.state), id:\(event.identifier), date:\(event.date)")
+                }
+            }
+        }
     }
 
 }
