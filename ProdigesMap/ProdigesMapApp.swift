49b6aafb0601e8342f8ebf033bdded6e9eb81910
Etienne Vautherin <etienne@vautherin.com>
2024-01-31 14:29:28 +0100

    Initial Commit

---
+++ ProdigesMap/ProdigesMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdigesMapApp.swift
+//  ProdigesMap
+//
+//  Created by Etienne Vautherin on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProdigesMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
