f0a8a849d4c838b04ac15d33b9e542ff46edfa64
Etienne Vautherin <etienne@vautherin.com>
2024-02-06 11:54:01 +0100

    LoginView content

--- ProdigesMap/LoginView.swift
+++ ProdigesMap/LoginView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct LoginView: View {
+
     @Environment(\.dismiss) var dismiss
+    @State private var name = ""
+    @State private var password = ""
 
     var body: some View {
-        Button("Press to dismiss") {
-            dismiss()
+        NavigationView {
+            VStack {
+                TextField("Nom", text: $name)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Mot de passe", text: $password)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button("Se connecter") {
+                    // Logique ici
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+            .navigationTitle("Connectez-vous")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Annuler") {
+                        dismiss()
+                    }
+                }
+            }
         }
+        .navigationViewStyle(.stack)
     }
 }
+
 
 #Preview {
     LoginView()
